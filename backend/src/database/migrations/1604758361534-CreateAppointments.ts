697031b88b16cf491b9bd7f69cbcf75c085f1311
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-11-07 12:11:15 -0300

    Atualiza AppointmentsRepository para utilizar o repositório do TypeORM

--- backend/src/database/migrations/1604758361534-CreateAppointments.ts
+++ backend/src/database/migrations/1604758361534-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1604758361534
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
