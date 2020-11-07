b5db64c69bea672f1912b604bb219ec638640697
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-11-07 19:03:12 -0300

    Cria relacionamento de Provider com Appointments

--- backend/src/database/migrations/1604758361534-CreateAppointments.ts
+++ backend/src/database/migrations/1604758361534-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1604758361534
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
