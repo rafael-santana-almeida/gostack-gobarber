a9c1c133dfc953f1b4ed4d6468ce33b6a2d4167c
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-11-30 23:27:32 -0300

    Exibindo mensagem de erro do formulário

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -20,7 +20,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsfilled] = useState(false);
-  const { fieldName, defaultValue, registerField } = useField(name);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
@@ -50,6 +50,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         type="text"
         {...rest}
       />
+
+      {error}
     </Container>
   );
 };
