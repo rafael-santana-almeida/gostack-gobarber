e7f6bf2ef5d6ab284789e99aebad774ea3f90e25
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-12-07 21:38:52 -0300

    Criado Hook e context de Toast

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,19 +3,15 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import ToastComponent from './components/ToastComponent';
-
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
-
-    <ToastComponent />
+    </AppProvider>
 
     <GlobalStyle />
   </>
---
+++ web/src/hooks/toast.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useCallback, useContext } from 'react';
+
+import ToastComponent from '../components/ToastComponent';
+
+interface ToastContextData {
+  addToast(): void;
+  removeToast(): void;
+}
+
+const ToastContext = createContext<ToastContextData>({} as ToastContextData);
+
+const ToastProvider: React.FC = ({ children }) => {
+  const addToast = useCallback(() => {
+    console.log('addToast');
+  }, []);
+
+  const removeToast = useCallback(() => {
+    console.log('removeToast');
+  }, []);
+
+  return (
+    <ToastContext.Provider value={{ addToast, removeToast }}>
+      {children}
+
+      <ToastComponent />
+    </ToastContext.Provider>
+  );
+};
+
+function useToast(): ToastContextData {
+  const context = useContext(ToastContext);
+
+  if (!context) {
+    throw new Error('useToast must be used whitin a ToastProvider');
+  }
+
+  return context;
+}
+
+export { ToastProvider, useToast };
