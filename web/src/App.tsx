c4de8a09bddf474ce866a06987ed39c1b2ed58e7
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-12-07 21:12:11 -0300

    Layout/estrutura de Toasts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import ToastComponent from './components/ToastComponent';
+
 import GlobalStyle from './styles/global';
 
 import { AuthProvider } from './hooks/AuthContext';
@@ -12,6 +14,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastComponent />
 
     <GlobalStyle />
   </>
