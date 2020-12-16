edf0d6cecc7daa4949e7c9fa1c195131504fd236
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-12-16 20:35:47 -0300

    Adicionando react-spring para animações e animando entrada e saída do Toast

--- web/src/components/ToastContainer/index.tsx
+++ web/src/components/ToastContainer/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 
-import { ToastMassage } from '../../hooks/toast';
 import Toast from './Toast';
 
+import { ToastMassage } from '../../hooks/toast';
 import { Container } from './styles';
 
 interface ToastContainerProps {
@@ -10,10 +11,19 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
   return (
     <Container>
-      {messages.map(message => (
-        <Toast key={message.id} message={message} />
+      {messagesWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} message={item} style={props} />
       ))}
     </Container>
   );
