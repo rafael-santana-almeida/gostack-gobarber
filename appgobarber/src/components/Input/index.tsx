23a91f6b349dc38fe716bf6a3e00548b425b5c8f
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-12-23 08:56:58 -0300

    Correção de erros de Spreading e children

--- appgobarber/src/components/Input/index.tsx
+++ appgobarber/src/components/Input/index.tsx
@@ -10,7 +10,7 @@ interface InputProps extends TextInputProps {
 
 const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
   <Container>
-    <Icon name={icon} size={20} color="#66630" />
+    <Icon name={icon} size={20} color="#666360" />
 
     <TextInput placeholderTextColor="#666360" {...rest} />
   </Container>
