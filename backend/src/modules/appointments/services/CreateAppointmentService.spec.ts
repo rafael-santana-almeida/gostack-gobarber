2143810b182f1fceb50b959b0d8db5079b12cacf
Rafael Santana <rafaelsantana.devweb@gmail.com>
2020-12-30 22:20:26 -0300

    Iniciando TDD

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(2 + 3).toBe(5);
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(2 + 3).toBe(5);
+  // });
+});
