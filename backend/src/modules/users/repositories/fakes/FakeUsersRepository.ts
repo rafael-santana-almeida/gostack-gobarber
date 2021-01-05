6033b9fe0ef264c90d91e9ac92811105e7c1a131
Rafael Santana <rafaelsantana.devweb@gmail.com>
2021-01-05 20:03:36 -0300

    Continuando o TDD de Recuperação de senha

--- backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -5,7 +5,7 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '../../infra/typeorm/entities/User';
 
-class UserRepository implements IUsersRepository {
+class FakeUserRepository implements IUsersRepository {
   private users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
@@ -39,4 +39,4 @@ class UserRepository implements IUsersRepository {
   }
 }
 
-export default UserRepository;
+export default FakeUserRepository;
