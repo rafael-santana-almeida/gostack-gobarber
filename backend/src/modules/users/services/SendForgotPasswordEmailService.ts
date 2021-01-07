10995dd63eda981b39d0a8e06d3c0ed7212021c0
Rafael Santana <rafaelsantana.devweb@gmail.com>
2021-01-06 22:01:03 -0300

    Aplicando conceitos de TDD para desenvolver funcionalidade de Reset de Senha

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,7 +2,6 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
-// import User from '@modules/users/infra/typeorm/entities/User';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokensRepository from '../repositories/IUserTokensRepository';
