753b348ea8bc8a3cf999459b5cc23b4958c1e83d
Rafael Santana <rafaelsantana.devweb@gmail.com>
2021-01-19 20:34:47 -0300

    Resolvendo conflito de agendamentos no mesmo horário

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -49,7 +49,8 @@ class CreateAppointmentService {
     }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
-      date,
+      appointmentDate,
+      provider_id,
     );
 
     if (findAppointmentInSameDate) {
