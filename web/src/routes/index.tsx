2311be017ad5498ec68127088ad46254d45cd2b4
Rafael Santana <rafaelsantana.devweb@gmail.com>
2021-01-20 21:51:11 -0300

    Criação da página de recuperação de senha + conexão com API

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -5,6 +5,7 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import forgotPassword from '../pages/ForgotPassword';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -12,6 +13,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={forgotPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
