bb8303a0c12889a4be09ec331dff329d167183e5
Rafael Santana <rafaelsantana.devweb@gmail.com>
2021-02-13 15:36:34 -0300

    Crando tela de perfil

--- appgobarber/src/components/Input/index.tsx
+++ appgobarber/src/components/Input/index.tsx
@@ -14,6 +14,7 @@ import { Container, TextInput, Icon } from './styles';
 interface InputProps extends TextInputProps {
   name: string;
   icon: string;
+  containerStyle?: {};
 }
 
 interface InputValueReference {
@@ -25,7 +26,9 @@ interface InputRef {
 }
 
 const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
-  { name, icon, ...rest },
+  {
+    name, icon, containerStyle = {}, ...rest
+  },
   ref,
 ) => {
   const inputElementRef = useRef<any>(null);
@@ -61,7 +64,7 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = (
   }));
 
   return (
-    <Container isFocused={isFocused} isErrored={!!error}>
+    <Container style={containerStyle} isFocused={isFocused} isErrored={!!error}>
       <Icon
         name={icon}
         size={20}
